584a2d271a5cc3442c8bffe691386ddbe5df2d7c
ayushchy <aachoudhary@mitaoe.ac.in>
2021-01-24 23:31:03 +0530

    Update binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -26,7 +26,7 @@ int binarySearch(int arr[], int l, int r, int x)
 
 int main(void) 
 { 
-	int arr[] = { 2, 3, 4, 10, 40 }; 
+	int arr[] = { 2, 3, 6, 10, 40, 33, 9, 7, 4, 82 }; 
 	int x = 10; 
 	int n = sizeof(arr) / sizeof(arr[0]); 
 	int result = binarySearch(arr, 0, n - 1, x); 
